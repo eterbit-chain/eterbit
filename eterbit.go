3cf0e8945ebdc475bbb1bab2cc35fac972e69272
Sawano <aldianokto@gmail.com>
2026-08-05 22:04:32 +0700

    Change send amount to Float64 for decimal precision
    Updated send amount parameter to use Float64 for decimal support and converted the amount to basic units before sending.

--- eterbit.go
+++ eterbit.go
@@ -50,7 +50,7 @@ func main() {
 	walletLabel := walletCreateCmd.String("label", "Default Account", "Label description for the new multi-wallet account")
 
 	sendRecipient := sendCmd.String("to", "", "Recipient destination address")
-	sendAmount := sendCmd.Uint64("amount", 0, "Transfer value amount")
+	sendAmount := sendCmd.Float64("amount", 0, "Transfer value amount in coins")
 	sendFee := sendCmd.Uint64("fee", 2, "Transaction fee")
 	sendSenderAddr := sendCmd.String("from", "", "Specific sender account address within wallet.dat")
 
@@ -85,7 +85,9 @@ func main() {
 	case "send":
 		// Parse transaction transfer parameters and invoke the modular CLI transaction dispatch handler.
 		sendCmd.Parse(os.Args[2:])
-		cli.HandleSendTx(*sendRecipient, *sendAmount, *sendFee, *sendSenderAddr)
+		// Konversi koin desimal ke satuan unit dasar (1 koin = 100,000,000 unit)
+		amountInUnits := uint64(*sendAmount * 100000000)
+		cli.HandleSendTx(*sendRecipient, amountInUnits, *sendFee, *sendSenderAddr)
 	case "node":
 		// Parse node server flag parameters and bootstrap the background P2P validator daemon service.
 		nodeCmd.Parse(os.Args[2:])
